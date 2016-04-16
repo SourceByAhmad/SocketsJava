e00718a7717c8287d442bb57fa1e14377b9a8de4
SourceByAhmad <ahmad.ali.iqbal@gmail.com>
2016-04-16 12:39:05 +1000

    client and server files added

---
+++ UDPServer.java
@@ -0,0 +1,52 @@
+/*
+ * FileName: UDPServer.java
+ * Source: https://github.com/SourceByAhmad/SocketsJava
+ * Description: Simplified UDP Server listening at port 5000
+ * License: Open for public use either as is or in modified form with reference to github.com/SourceByAhmad/SocketsJava repository
+ */
+import java.net.*;
+import java.io.*;
+
+
+class UDPServer 
+{
+   public static void main(String args[]) throws Exception
+      {
+      	
+      	 byte[] receive_data = new byte[1024];
+         byte[] send_data = new byte[1024];
+         
+         int recv_port;
+         
+         DatagramSocket server_socket = new DatagramSocket(5000);
+         
+         System.out.println ("UDPServer Waiting for client on port 5000");
+         
+              
+         while(true)
+         {
+          
+          DatagramPacket receive_packet = new DatagramPacket(receive_data,
+                                              receive_data.length);
+                                              
+                  server_socket.receive(receive_packet);
+                  
+                  String data = new String(receive_packet.getData(),0                                          ,0
+                                           ,receive_packet.getLength());
+                  
+                  InetAddress IPAddress = receive_packet.getAddress();
+                  
+                  recv_port = receive_packet.getPort();
+                  
+                  if (data.equals("q") || data.equals("Q"))
+                  break;
+                  
+                  else
+                  
+                  System.out.println("( " + IPAddress + " , " + recv_port 
+                  + " ) said :" + data );
+                  
+                  
+         }
+      }
+}
